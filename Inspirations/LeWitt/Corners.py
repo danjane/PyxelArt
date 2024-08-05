c6b8ebe197382e4f63e495763096e895fc056dab
danjane <jdtane@gmail.com>
2024-08-05 17:51:38 +0200

    More examples

--- Inspirations/LeWitt/Corners.py
+++ Inspirations/LeWitt/Corners.py
@@ -7,13 +7,11 @@ pyxel.colors[0] = 0x555555
 pyxel.colors[1] = 0xcccccc
 pyxel.cls(0)
 
-
-
-for x in [1, W/2, W-1]:
-    for y in [1, W/2, W-1]:
+for x in [1, W / 2, W - 1]:
+    for y in [1, W / 2, W - 1]:
         for i in range(24):
-            theta = i*360/24
-            r = pyxel.rndi(0, W//2)
-            pyxel.line(x, y, x + r*pyxel.cos(theta), y + r*pyxel.sin(theta), 1)
+            theta = i * 360 / 24
+            r = pyxel.rndi(0, W // 2)
+            pyxel.line(x, y, x + r * pyxel.cos(theta), y + r * pyxel.sin(theta), 1)
 
 pyxel.show()
