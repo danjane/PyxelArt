d66dfe39e9145c7d16ae0109cd63a68d68dd8393
danjane <jdtane@gmail.com>
2024-09-29 21:14:45 +0200

    Frize's Mailles updated with the simplified Bezier code

--- Inspirations/Frize/Mailles.py
+++ Inspirations/Frize/Mailles.py
@@ -1,40 +1,25 @@
 import pyxel
 import numpy as np
-import math
 
 W = 1600
 SEGMENTS = 1000
 LINES = 80
 
-def vector(cs):
-    return np.array(cs)
-
-
-def bezier_point(t, control_points):
-    """ Calculate the position of a point on the Bezier curve at parameter t """
-    n = len(control_points) - 1
-    p = vector([0., 0.])
-    # Calculate point on the Bezier curve using the Bernstein polynomial
-    for i, point in enumerate(control_points):
-        binomial_coeff = math.comb(n, i)
-        p += binomial_coeff * (t ** i) * ((1 - t) ** (n - i)) * point
+def bezier_point(vec_ctrls, t):
+    p = (1-t) ** 3 * vec_ctrls[0] + 3 * (1 - t) ** 2 * t * vec_ctrls[1] + 3 * (1 - t) * t ** 2 * vec_ctrls[2] + t ** 3 * vec_ctrls[3]
     return p
 
-
-def bezier_points(control_points, segments=SEGMENTS):
-    """ Draw a Bezier curve based on given control points """
-    vs = [vector(v) for v in control_points]
+def bezier_points(pt_ctrls):
+    vec_ctrls = np.array(pt_ctrls)
     points = []
-    for i in range(segments + 1):
-        t = i / segments
-        points.append(bezier_point(t, vs))
+    for i in range(SEGMENTS + 1):
+        t = i / SEGMENTS
+        points.append(bezier_point(vec_ctrls, t))
     return points
 
-
-def lines(points, colour):
-    for p in points:
-        pyxel.circ(*p, 4, colour)
-
+def trace(pt_ctrls, couleur):
+    for p in bezier_points(pt_ctrls):
+        pyxel.circ(*p, 4, couleur)
 
 pyxel.init(W, W)
 pyxel.cls(0)
@@ -53,8 +38,8 @@ ys = [W*y/sum(ys_unscaled) for y in ys_unscaled]
 y1 = 0
 y2 = 0
 for i in range(LINES):
-    ps = bezier_points([[0, y1], [W/2, y1], [W/2, y2], [W, y2]])
-    lines(ps, pyxel.rndi(1, 15))
+    ps = [[0, y1], [W/2, y1], [W/2, y2], [W, y2]]
+    trace(ps, pyxel.rndi(1, 15))
     y1 = y1 + ys[i]
     y2 = y2 + ys[-i-1]
 
